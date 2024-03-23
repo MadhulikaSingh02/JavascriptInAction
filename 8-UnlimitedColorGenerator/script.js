43a3670e1ec6d739b9bb6c906352ada9f4c06f5f
Madhulika Singh <MadhulikaSingh02@gmail.com>
2024-03-22 19:33:28 -0700

    console.log removed

--- 8-UnlimitedColorGenerator/script.js
+++ 8-UnlimitedColorGenerator/script.js
@@ -2,7 +2,7 @@ const start = document.querySelector("#start");
 const stop = document.querySelector("#stop");
 const main = document.querySelector("main");
 const colorDiv = document.querySelector(".color");
-console.log(colorDiv);
+
 let timer;
 start.addEventListener(
   "click",
